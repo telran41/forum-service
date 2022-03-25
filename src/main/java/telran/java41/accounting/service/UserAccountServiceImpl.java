0646e572109df9e45ce15f4e39ee34fe8c2bbe87
EddFish <edwardlfish@gmail.com>
2022-03-25 08:52:32 +0300

    Complete AccountServiceImpl

--- src/main/java/telran/java41/accounting/service/UserAccountServiceImpl.java
+++ src/main/java/telran/java41/accounting/service/UserAccountServiceImpl.java
@@ -42,26 +42,44 @@ public class UserAccountServiceImpl implements UserAccountService {
 
 	@Override
 	public UserAccountResponseDto removeUser(String login) {
-		// TODO Auto-generated method stub
-		return null;
+		UserAccount userAccount = repository.findById(login).orElseThrow(() -> new UserNotFoundException());
+		repository.deleteById(login);
+		return modelMapper.map(userAccount, UserAccountResponseDto.class);
 	}
 
 	@Override
 	public UserAccountResponseDto editUser(String login, UserUpdateDto updateDto) {
-		// TODO Auto-generated method stub
-		return null;
+		UserAccount userAccount = repository.findById(login).orElseThrow(() -> new UserNotFoundException());
+		if (updateDto.getFirstName() != null) {
+			userAccount.setFirstName(updateDto.getFirstName());
+		}
+		if (updateDto.getLastName() != null) {
+			userAccount.setLastName(updateDto.getLastName());
+		}
+		repository.save(userAccount);
+		return modelMapper.map(userAccount, UserAccountResponseDto.class);
 	}
 
 	@Override
 	public RolesResponseDto changeRolesList(String login, String role, boolean isAddRole) {
-		// TODO Auto-generated method stub
-		return null;
+		UserAccount userAccount = repository.findById(login).orElseThrow(() -> new UserNotFoundException());
+		boolean res;
+		if (isAddRole) {
+			res = userAccount.addRole(role.toUpperCase());
+		} else {
+			res = userAccount.removeRole(role.toUpperCase());
+		}
+		if (res) {
+			repository.save(userAccount);
+		}	
+		return modelMapper.map(userAccount, RolesResponseDto.class);
 	}
 
 	@Override
 	public void changePassword(String login, String newPassword) {
-		// TODO Auto-generated method stub
-
+		UserAccount userAccount = repository.findById(login).orElseThrow(() -> new UserNotFoundException());
+		userAccount.setPassword(newPassword);
+		repository.save(userAccount);
 	}
 
 }
